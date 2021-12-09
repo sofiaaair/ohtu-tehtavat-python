56e89684d7ef0f385bfa8f62bf9ef0277d339b44
sofiaaair <sofia.airas@hotmail.com>
2021-12-09 21:05:33 +0200

    pieniä muutoksia

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -13,3 +13,9 @@ class TestOstoskori(unittest.TestCase):
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavarioita_korissa(), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korin_hinta_on_oikea(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
